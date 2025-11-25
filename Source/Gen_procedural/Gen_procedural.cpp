01d0283c9ee9ec2be967e114b4ee0ade0fc0be75
Ase <tdruesne05@gmail.com>
2025-11-25 17:55:02 +0100

    Change project files to new Unreal Project

--- Source/Gen_procedural/Gen_procedural.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Gen_procedural.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Gen_procedural, "Gen_procedural" );
